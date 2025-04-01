8c682e93f0185d4e42b0224156a179ac7a6b7684
IgloBruh <DmShadow83@gmail.com>
2025-04-01 17:06:41 +0300

    Roll() updated

--- Modules/Misc.py
+++ Modules/Misc.py
@@ -1,6 +1,11 @@
 from random import randint
 
-def Roll(rollcode='1d20'):
+def Roll(rollcode='1d20', mod=None):
+    if mod != 0 and mod is not None:
+        rolls = [Roll(rollcode, mod=0), Roll(rollcode, mod=0)]
+        print(f"Raw: [{rolls[0]}] [{rolls[1]}]")
+        print(f"Final: {min(rolls) if mod == -1 else max(rolls)}")
+        return min(rolls) if mod == -1 else max(rolls)
     parts = list(rollcode.split("+"))
     result = 0
     for _ in parts:
@@ -11,4 +16,6 @@ def Roll(rollcode='1d20'):
         else:
             subresult = int(_)
         result += subresult
+    if mod is None:
+        print(f"Final: {result}")
     return result
